d14d9dd22c958af49fe4e913dec74ae5e7706ed0
AndresXweb <william03092014@hotmail.com>
2025-09-02 07:12:08 -0500

    add button_update

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,66 @@ import TareaItem from "./TareaItem";
 import { useState } from "react";
 
 export default function App() {
-
   const [tareas, setTareas] = useState([]);
-
   const [input, setInput] = useState("");
 
   const agregarTarea = () => {
-    if(input.trim() ){
-      setTareas([...tareas, {id: Date.now(), texto: input.trim(), completed: false}]);
+    if (input.trim()) {
+      setTareas([...tareas, { id: Date.now(), text: input.trim(), completed: false }]);
       setInput("");
-    };
-  }
+    }
+  };
 
-  const toggleComplited = (id) => {
+  const toggleCompleted = (id) => {
     setTareas(
-      tareas.map(tarea => 
-        tarea.id === id ? {...tarea, completed: !tarea.completed} : tarea
+      tareas.map((tarea) =>
+        tarea.id === id ? { ...tarea, completed: !tarea.completed } : tarea
       )
     );
-  }
+  };
 
-  const toggleEliminated = (id) => {
+  const eliminarTarea = (id) => {
     setTareas(tareas.filter((tarea) => tarea.id !== id));
   };
-  
+
+  const editarTarea = (id, nuevoTexto) => {
+    setTareas(
+      tareas.map((tarea) =>
+        tarea.id === id ? { ...tarea, text: nuevoTexto } : tarea
+      )
+    );
+  };
+
   return (
     <div className="max-w-md mx-auto mt-10 p-2 rounded shadow">
       <h1 className="text-3xl font-bold mb-5 text-center">LISTA DE TAREAS</h1>
-      <div className="flex gab-3 mb-5">
-        <input type="text" className="flex-1 p-2 border-rounded" value={input} onChange={(e) => setInput(e.target.value)} placeholder=""/>
-        <button className="bg-blue-500 text-white px-4 p-y-2 rounded" onClick={agregarTarea}>Añadir tareas</button>
+      <div className="flex gap-3 mb-5">
+        <input
+          className="flex-1 p-2 border rounded"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Añadir Tarea"
+        />
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+          onClick={agregarTarea}
+        >
+          Añadir Tarea
+        </button>
       </div>
 
       <div className="space-y-2">
-        {tareas.map((tarea) => (<TareaItem key={tarea.id} tarea={tarea} toggleComplited={toggleComplited} toggleEliminated={toggleEliminated}/>))}
+        {tareas.map((tarea) => (
+          <TareaItem
+            key={tarea.id}
+            tarea={tarea}
+            toggleCompleted={toggleCompleted}
+            eliminarTarea={eliminarTarea}
+            editarTarea={editarTarea}
+          />
+        ))}
       </div>
     </div>
   );
-
 }
